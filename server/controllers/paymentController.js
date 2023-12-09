7ab02229cef6cd2bb6feb626db84ffb80504b8bf
HoffmanSan <133865170+HoffmanSan@users.noreply.github.com>
2023-12-09 11:08:23 +0100

    Update paymentController.js
    changed url for success_url and cancel_url

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -38,8 +38,8 @@ const createCheckoutSession = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
       line_items: lineItems,
-      success_url: `${process.env.API_SERVER_URI}api/payments/payment-successful?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.CLIENT_URI}failure`,
+      success_url: `${process.env.API_SERVER_URI}/api/payments/payment-successful?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.CLIENT_URI}/failure`,
       client_reference_id: user.cartId
     });
 
